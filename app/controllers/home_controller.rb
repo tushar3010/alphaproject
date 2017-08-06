f5191ed68e3bf27689a2a06a35e42d092b17c024
tushar <kalkal3010@gmail.com>
2017-08-07 02:14:17 +0530

    some error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,21 @@ class HomeController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
-		
+		@questions = Question.includes(:answers,:question_upvotes).all
+		@questions = Questions.all
 	end
 
 	def ques
+		content = params[:content]
+		current_user.questions.create(:content => content)
+		redirect_to '/'
+	end
 
+	def ans
+		answer = params[:answer]
+		ques = current_user.questions.find(params[:ques])
+		ques.answers.create(:answer => answer)
+		redirect_to '/'
 	end
 
 end
