62a025f222fdb392d69395bcf7a5c3602cf12468
tushar <kalkal3010@gmail.com>
2017-08-09 00:31:39 +0530

    after basic styling

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,6 @@ class Question < ActiveRecord::Base
 	belongs_to :user
 	has_many :answers
 	has_many :upvotes, :as=>:upvotable
-    validates :content, presence: true, length: {maximum: 140}
+    validates :content, presence: true, length: {maximum: 800}
 
 end
